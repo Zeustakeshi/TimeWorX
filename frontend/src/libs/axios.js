d9174c73eb11646d4d0f99a3dc92707157bdf748
Minh Hieu <95726917+Zeustakeshi@users.noreply.github.com>
2025-11-20 13:47:35 +0700

    Update axios.js

--- frontend/src/libs/axios.js
+++ frontend/src/libs/axios.js
@@ -1,30 +1,31 @@
-import Axios from 'axios'
+// lib/axios.js
+import Axios from 'axios';
+
+let csrfInitialized = false;
 
 const axios = Axios.create({
-    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
-    headers: {
-        'X-Requested-With': 'XMLHttpRequest',
-    },
-    withCredentials: true,
-    withXSRFToken: true,
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  headers: {
+    'X-Requested-With': 'XMLHttpRequest'
+  },
+  withCredentials: true,
+  withXSRFToken: true
 })
 
-// Interceptor cho mọi request
 axios.interceptors.request.use(async (config) => {
   if (!csrfInitialized) {
     try {
-      // Gọi endpoint Sanctum để khởi tạo CSRF token
       await Axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/sanctum/csrf-cookie`, {
-        withCredentials: true,
-      });
-      csrfInitialized = true;
-      console.log('✅ CSRF token initialized');
+        withCredentials: true
+      })
+      csrfInitialized = true
+      // console.log('✅ CSRF token initialized')
     } catch (error) {
-      console.error('❌ Failed to initialize CSRF token', error);
-      throw error;
+      // console.error('❌ Failed to initialize CSRF token', error)
+      throw error
     }
   }
-  return config;
-});
+  return config
+})
 
 export default axios
