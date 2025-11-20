e75dbb29b21e754840f9b7131fb6b67ca7524bf3
Minh Hieu <95726917+Zeustakeshi@users.noreply.github.com>
2025-11-20 13:49:08 +0700

    Update axios.js

--- frontend/src/libs/axios.js
+++ frontend/src/libs/axios.js
@@ -1,7 +1,7 @@
-// lib/axios.js
-import Axios from 'axios';
+// ./src/libs/axios.js
+import Axios from 'axios'
 
-let csrfInitialized = false;
+let csrfInitialized = false
 
 const axios = Axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
@@ -14,16 +14,10 @@ const axios = Axios.create({
 
 axios.interceptors.request.use(async (config) => {
   if (!csrfInitialized) {
-    try {
-      await Axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/sanctum/csrf-cookie`, {
-        withCredentials: true
-      })
-      csrfInitialized = true
-      // console.log('✅ CSRF token initialized')
-    } catch (error) {
-      // console.error('❌ Failed to initialize CSRF token', error)
-      throw error
-    }
+    await Axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/sanctum/csrf-cookie`, {
+      withCredentials: true
+    })
+    csrfInitialized = true
   }
   return config
 })
